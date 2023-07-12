f8a4aa4140e7fef79552914f1e63db88ef1f919e
Luciano Mallo <malloluciano@gmail.com>
2023-07-12 14:25:37 +0200

    Add pagination

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,25 +2,47 @@ import './Products.css'
 import { AddToCartIcon, RemoveFromCartIcon } from '../Icons/Icons'
 import { useCart } from '../../hooks/useCart'
 import ProductImages from './ProductImages/ProductImages'
+import { useState } from 'react'
 
 export default function Products ({ products }) {
   const productsToShow = 10
   const { addToCart, removeFromCart, cart } = useCart()
+  const [pageOfProducts, setPageOfProducts] = useState(1)
+  const [startIndex, endIndex] = [(pageOfProducts - 1) * productsToShow, pageOfProducts * productsToShow]
+  const totalPagesOfProducts = Math.round(products.length / productsToShow)
 
   const checkProductInCart = product => {
     return cart.some(item => item.id === product.id)
   }
 
+  if (pageOfProducts > totalPagesOfProducts) setPageOfProducts(1)
+
+  function handlePrev () {
+    if (pageOfProducts === 1) {
+      return null
+    }
+    setPageOfProducts(pageOfProducts - 1)
+  }
+  function handleNext () {
+    if (pageOfProducts === products.length / productsToShow) {
+      return null
+    }
+
+    setPageOfProducts(pageOfProducts + 1)
+  }
+
   return (
     <main className='products'>
+
       <ul>
-        {products.slice(0, productsToShow).map(product => {
+        {products.slice(startIndex, endIndex).map(product => {
           const isProductInCart = checkProductInCart(product)
           return (
             <li key={product.id}>
+              <div>{product.title}</div>
               <ProductImages product={product} />
               <div>
-                <div>{product.title} - $ {product.price}</div>
+                <div>$ {product.price}</div>
               </div>
               <div>
                 <button
@@ -33,10 +55,24 @@ export default function Products ({ products }) {
                     : <AddToCartIcon />}
                 </button>
               </div>
+              <div className='toggle-description'>Description
+                <div className='product-description'>
+                  <p>
+                    {product.description}
+                  </p>
+                </div>
+              </div>
             </li>
           )
         })}
       </ul>
+      <div>
+        <div className='pages-counter'>
+          <button className='prev-Pages' onClick={() => handlePrev()}>←</button>
+          <div>Page {pageOfProducts} of {totalPagesOfProducts}</div>
+          <button className='next-Pages' onClick={() => handleNext()}>→</button>
+        </div>
+      </div>
     </main>
   )
 }
